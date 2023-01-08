65566ff1bccfad3b9a858014052e3180a527548c
Swiichy <adlpromail@gmail.com>
2023-01-08 10:12:46 +0100

    feat: mobile menu implementation -- probably need to refact

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("main.js");
+const sidenav = document.getElementById("mySidenav");
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
+
+openBtn.addEventListener("click", function () {
+  sidenav.classList.toggle("active");
+  openBtn.classList.toggle("close");
+});
