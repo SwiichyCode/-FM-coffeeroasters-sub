c15c73490a4d66352dc5d0a259f44d4af34876b8
Swiichy <adlpromail@gmail.com>
2023-01-08 22:23:07 +0100

    feat: more logic for mobile nav event

--- main.js
+++ main.js
@@ -6,3 +6,23 @@ openBtn.addEventListener("click", function () {
   sidenav.classList.toggle("active");
   openBtn.classList.toggle("close");
 });
+
+// close the navbar on scroll
+document.addEventListener("scroll", function () {
+  if (window.scrollY > 150) {
+    sidenav.classList.remove("active");
+    openBtn.classList.remove("close");
+  }
+});
+
+// dectecting click outside of the sidenav
+document.addEventListener("click", function (e) {
+  if (sidenav.classList.contains("active")) {
+    if (!sidenav.contains(e.target) && !openBtn.contains(e.target)) {
+      sidenav.classList.remove("active");
+      openBtn.classList.remove("close");
+    }
+  }
+});
+
+// Need to fix size of the sidenav on mobile for better UX
