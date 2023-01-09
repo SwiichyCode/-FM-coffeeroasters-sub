fa4b9c10b111b91e5abb0fee3e33d37f8291940f
Swiichy <adlpromail@gmail.com>
2023-01-09 18:17:34 +0100

    feat: Finalizing Form Logic

--- main.js
+++ main.js
@@ -25,4 +25,47 @@ document.addEventListener("click", function (e) {
   }
 });
 
-// Need to fix size of the sidenav on mobile for better UX
+const summary = [[], [], [], [], []];
+const summaryEl = document.getElementById("summary");
+const summaryItems = document.querySelectorAll(".summary-item");
+const itemsList = document.querySelectorAll(".items-list");
+const items = document.querySelectorAll(".item");
+const selected = document.querySelector(".selected");
+const initialSummary = `
+  "I drink my coffee as <span class="summary-item">__</span>, with a <span class="summary-item">__</span> type of bean. <span class="summary-item">__</span> ground ala <span class="summary-item">__</span>, sent to me <span class="summary-item">__</span>."`;
+summaryEl.innerHTML = initialSummary;
+
+items.forEach((item) => {
+  item.addEventListener("click", function () {
+    item.classList.toggle("selected");
+
+    // get the index of the parent ul
+    const parentUlIndex = Array.from(itemsList).indexOf(item.parentNode);
+    summary[parentUlIndex].push(item.firstElementChild.textContent);
+    if (summary[parentUlIndex].length > 1) {
+      summary[parentUlIndex].shift();
+    }
+
+    const parentUl = item.parentNode;
+    const ulItems = parentUl.querySelectorAll(".item");
+
+    ulItems.forEach((ulItem) => {
+      ulItem.classList.remove("selected");
+    });
+
+    item.classList.add("selected");
+
+    summaryEl.innerHTML = `
+      "I drink  my coffee as <span class="summary-item">${
+        summary[0].length ? summary[0] : "__"
+      }</span>, with a <span class="summary-item">${
+      summary[1].length ? summary[1] : "__"
+    }</span> type of bean. <span class="summary-item">${
+      summary[2].length ? summary[2] : "__"
+    }</span> ground ala <span class="summary-item">${
+      summary[3].length ? summary[3] : "__"
+    }</span>, sent to me <span class="summary-item">${
+      summary[4].length ? summary[4] : "__"
+    }</span>." `;
+  });
+});
