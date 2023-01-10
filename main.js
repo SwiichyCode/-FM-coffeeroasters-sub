a08a1ece95fba1f0f194b5f1943ced17be24445c
Swiichy <adlpromail@gmail.com>
2023-01-10 23:09:40 +0100

    feat: build modal compenent

--- main.js
+++ main.js
@@ -23,6 +23,14 @@ document.addEventListener("click", function (e) {
   }
 });
 
+const labels = document.querySelectorAll(".select__label");
+
+labels.forEach((label) => {
+  label.addEventListener("click", function () {
+    this.nextElementSibling.classList.toggle("active");
+  });
+});
+
 //-------------------//
 
 const summary = [[], [], [], [], []];
@@ -31,9 +39,26 @@ const summaryItems = document.querySelectorAll(".summary-item");
 const itemsList = document.querySelectorAll(".items-list");
 const items = document.querySelectorAll(".item");
 const selected = document.querySelector(".selected");
+
 const initialSummary = `
   "I drink my coffee as <span class="summary-item">__</span>, with a <span class="summary-item">__</span> type of bean. <span class="summary-item">__</span> ground ala <span class="summary-item">__</span>, sent to me <span class="summary-item">__</span>."`;
 summaryEl.innerHTML = initialSummary;
+
+function generateSummaryHTML(summary) {
+  return `
+    "I drink my coffee as <span class="summary-item">${
+      summary[0].length ? summary[0] : "__"
+    }</span>, with a <span class="summary-item">${
+    summary[1].length ? summary[1] : "__"
+  }</span> type of bean. <span class="summary-item">${
+    summary[2].length ? summary[2] : "__"
+  }</span> ground ala <span class="summary-item">${
+    summary[3].length ? summary[3] : "__"
+  }</span>, sent to me <span class="summary-item">${
+    summary[4].length ? summary[4] : "__"
+  }</span>."
+  `;
+}
 
 items.forEach((item) => {
   item.addEventListener("click", function () {
@@ -54,25 +79,49 @@ items.forEach((item) => {
 
     item.classList.add("selected");
 
-    summaryEl.innerHTML = `
-      "I drink  my coffee as <span class="summary-item">${
-        summary[0].length ? summary[0] : "__"
-      }</span>, with a <span class="summary-item">${
-      summary[1].length ? summary[1] : "__"
-    }</span> type of bean. <span class="summary-item">${
-      summary[2].length ? summary[2] : "__"
-    }</span> ground ala <span class="summary-item">${
-      summary[3].length ? summary[3] : "__"
-    }</span>, sent to me <span class="summary-item">${
-      summary[4].length ? summary[4] : "__"
-    }</span>." `;
+    summaryEl.innerHTML = generateSummaryHTML(summary);
   });
 });
 
-const labels = document.querySelectorAll(".select__label");
+const submitBtn = document.getElementById("submit");
 
-labels.forEach((label) => {
-  label.addEventListener("click", function () {
-    this.nextElementSibling.classList.toggle("active");
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // if (summary.map((item) => item.length).includes(0)) {
+  //   alert("Please select all options");
+  //   return;
+  // }
+
+  // create a modal
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+  modal.innerHTML = `
+
+    <div class="modal">
+      <div class="modal__header"> 
+        <h3 class="modal__title">Order Summary</h3>
+      </div>
+      <p class="modal__text">You ordered a <span class="modal-summary">${summary[0]}</span> coffee with a <span class="modal-summary">${summary[1]}</span> type of bean. <span class="modal-summary">${summary[2]}</span> ground ala <span class="modal-summary">${summary[3]}</span>, sent to me <span class="modal-summary">${summary[4]}</span>.</p>
+      <button class="btn">Checkout - $14.00 / month</button>
+    </div>
+  `;
+  document.body.appendChild(modal);
+
+  // apply backdrop shadow filter to body
+  const container = document.querySelector(".container");
+  // create a backdrop
+  const backdrop = document.createElement("div");
+
+  backdrop.classList.add("backdrop");
+  container.appendChild(backdrop);
+
+  // close modal on backdrop click
+  container.addEventListener("click", function (e) {
+    if (e.target.classList.contains("backdrop")) {
+      modal.remove();
+      backdrop.remove();
+      container.classList.remove("backdrop");
+    }
   });
 });
